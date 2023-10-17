6713a5ab5ba43b14eea6bf024e0d3103b530101c
Fayssaldagod <you@example.com>
2023-10-17 16:31:16 -0700

    printf

---
+++ extra-funcs.c
@@ -0,0 +1,121 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string along with a new line
+ * @str: The string to be printed
+ *
+ * Return: None
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - Writes the C character to stdout
+ * @c: The character to be printed
+ *
+ * Return: if it's success 1.
+ * if it's error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * convert - Function converter
+ * @num: Number
+ * @base: Base
+ * @flags: Argument flags
+ * @params: Paramater Structure
+ *
+ * Return: A String
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+	(void)params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * print_unsigned - Prints unsigned integer numbers
+ * @ap: Argument Pointers
+ * @params: The struct Parameters
+ *
+ * Return: Bytes to be printed
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+}
+
+
+
+/**
+ * print_address - Prints an address
+ * @ap: Argument pointer
+ * @params: The parameters struct
+ *
+ * Return: Bytes that will  be printed
+ */
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
+
---
+++ more-funcs.c
@@ -0,0 +1,135 @@
+#include "main.h"
+
+/**
+ * print_from_to - Prints a range of Characters addresses
+ * @start: Starting
+ * @stop: Stopping
+ * @except: Except
+ *
+ * Return: Number of bytes to print
+ */
+int print_from_to(char *start, char *stop, char *except)
+{
+	int sum = 0;
+
+	while (start <= stop)
+	{
+		if (start != except)
+			sum += _putchar(*start);
+		start++;
+	}
+	return (sum);
+}
+
+/**
+ * print_rev - Prints the reversed string
+ * @ap: String
+ * @params: The parameters structures
+ *
+ * Return: Number of Bytes to print
+ */
+int print_rev(va_list ap, params_t *params)
+{
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+	(void)params;
+
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
+	}
+	return (sum);
+}
+
+/**
+ * print_rot13 - Prints the String in rot13
+ * @ap: String
+ * @params: The parameters structures
+ *
+ * Return: Number of bytes that will be printed
+ */
+int print_rot13(va_list ap, params_t *params)
+{
+	int i, index;
+	int count = 0;
+	char arr[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(ap, char *);
+	(void)params;
+
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+		    || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			count += _putchar(arr[index]);
+		}
+		else
+			count += _putchar(a[i]);
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * init_params - Clears the Structures fields and resets buff
+ * @params: The Parameters structures
+ * @ap: The Argument pointers
+ *
+ * Return: None
+ */
+void init_params(params_t *params, va_list ap)
+{
+	params->unsign = 0;
+
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
+
+	params->width = 0;
+	params->precision = UINT_MAX;
+
+	params->h_modifier = 0;
+	params->l_modifier = 0;
+	(void)ap;
+}
+
+/**
+ * get_precision - will get the precision from the string format
+ * @p: The strings format
+ * @params: The Parameters structures
+ * @ap: The argument pointer
+ *
+ * Return: A new pointer
+ */
+char *get_precision(char *p, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		d = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
+	}
+	params->precision = d;
+	return (p);
+}
+
+
